e02956c9cfcc3756daa2612db47c2ecdb5d47547
MacMini-03 <mac03@pc3.local>
2018-03-27 15:15:19 +0700

    Initial Commit

---
+++ MiniProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniProject
+//
+//  Created by MacMini-03 on 3/27/18.
+//  Copyright © 2018 Bootcamp141. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
